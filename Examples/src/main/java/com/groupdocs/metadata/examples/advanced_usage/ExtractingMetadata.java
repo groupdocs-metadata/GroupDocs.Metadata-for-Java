0c7bce9f647beaafcc06ae270431ce610fdb37e1
Sergey Voloshin <sergey.a.voloshin@gmail.com>
2020-05-08 14:16:57 +0700

    Update some general code samples

--- Examples/src/main/java/com/groupdocs/metadata/examples/advanced_usage/ExtractingMetadata.java
+++ Examples/src/main/java/com/groupdocs/metadata/examples/advanced_usage/ExtractingMetadata.java
@@ -10,6 +10,7 @@ import com.groupdocs.metadata.core.IReadOnlyList;
 import com.groupdocs.metadata.core.MetadataProperty;
 import com.groupdocs.metadata.core.MetadataPropertyType;
 import com.groupdocs.metadata.examples.Constants;
+import com.groupdocs.metadata.examples.migration.working_with_regular_expressions.FindPropertiesByRegex;
 import com.groupdocs.metadata.search.FallsIntoCategorySpecification;
 import com.groupdocs.metadata.search.OfTypeSpecification;
 import com.groupdocs.metadata.search.Specification;
@@ -18,6 +19,8 @@ import com.groupdocs.metadata.tagging.Tags;
 import java.io.File;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This example demonstrates how to extract metadata properties by various criteria regardless of the file format.
@@ -42,6 +45,14 @@ public class ExtractingMetadata {
                     int year = Calendar.getInstance().get(Calendar.YEAR);
                     properties = metadata.findProperties(new OfTypeSpecification(MetadataPropertyType.DateTime).and(new ExtractingMetadata().new YearMatchSpecification(year)));
                     System.out.println("All datetime properties with the year value equal to the current year");
+                    for (MetadataProperty property : properties) {
+                        System.out.println(String.format("Property name: %s, Property value: %s", property.getName(), property.getValue()));
+                    }
+
+                    // Fetch all properties whose names match the specified regex
+                    Pattern pattern = Pattern.compile("^author|company|(.+date.*)$", Pattern.CASE_INSENSITIVE);
+                    properties = metadata.findProperties(new ExtractingMetadata().new RegexSpecification(pattern));
+                    System.out.println(String.format("All properties whose names match the following regex: %s", pattern.pattern()));
                     for (MetadataProperty property : properties) {
                         System.out.println(String.format("Property name: %s, Property value: %s", property.getName(), property.getValue()));
                     }
@@ -74,6 +85,20 @@ public class ExtractingMetadata {
                 return getValue() == calendar.get(Calendar.YEAR);
             }
             return false;
+        }
+    }
+
+    public class RegexSpecification extends Specification {
+        private Pattern pattern;
+
+        public RegexSpecification(Pattern pattern) {
+            this.pattern = pattern;
+        }
+
+        @Override
+        public boolean isSatisfiedBy(MetadataProperty metadataProperty) {
+            Matcher matcher = pattern.matcher(metadataProperty.getName());
+            return matcher.find();
         }
     }
 }
