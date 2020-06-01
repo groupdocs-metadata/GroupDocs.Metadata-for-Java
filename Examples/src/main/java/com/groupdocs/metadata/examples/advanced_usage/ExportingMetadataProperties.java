54cf2d7568b339b9127387f62bb567e3efa8f722
Sergey Voloshin <sergey.a.voloshin@gmail.com>
2020-06-01 17:10:28 +0700

    Fix code formatting

--- Examples/src/main/java/com/groupdocs/metadata/examples/advanced_usage/ExportingMetadataProperties.java
+++ Examples/src/main/java/com/groupdocs/metadata/examples/advanced_usage/ExportingMetadataProperties.java
@@ -1,7 +1,6 @@
 // <copyright company="Aspose Pty Ltd">
 //   Copyright (C) 2011-2020 GroupDocs. All Rights Reserved.
 // </copyright>
-
 package com.groupdocs.metadata.examples.advanced_usage;
 
 import com.groupdocs.metadata.Metadata;
@@ -15,12 +14,11 @@ import com.groupdocs.metadata.export.ExportManager;
  * This example demonstrates how to export metadata properties to an Excel workbook.
  */
 public class ExportingMetadataProperties {
+
     public static void run() {
-        try (Metadata metadata = new Metadata(Constants.InputDoc))
-        {
+        try (Metadata metadata = new Metadata(Constants.InputDoc)) {
             RootMetadataPackage root = metadata.getRootPackage();
-            if (root != null)
-            {
+            if (root != null) {
                 // Initialize the export manager with the root metadata package to export the whole metadata tree
                 ExportManager manager = new ExportManager(root);
 
